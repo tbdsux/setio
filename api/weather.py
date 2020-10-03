b672c9ac2467d7ebda13695ad24b9c7cc8cc64c3
TheBoringDude <iamcoderx@gmail.com>
2020-10-03 21:05:31 +0800

    Save the file

--- api/weather.py
+++ api/weather.py
@@ -1,2 +1,18 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+base_url = "http://api.openweathermap.org/data/2.5/weather?q="
 
+class Weather:
+    @staticmethod
+    def Get(city_id):
+        resp = requests.get(base_url + city_id + "&appid=" + os.getenv("OPENWEATHER")).json()
+
+        return [resp['name'], resp['sys']['country'], resp['weather'][0]['main'], resp['weather'][0]['description'], Weather.convert_temp(resp['main']['temp']), Weather.convert_temp(resp['main']['feels_like']), Weather.convert_temp(resp['main']['temp_min']), Weather.convert_temp(resp['main']['temp_max'])]
+
+    # conver the temperature from kelvin to celcius
+    @staticmethod
+    def convert_temp(kelvin_temp):
+        return round(kelvin_temp - 273.15)
