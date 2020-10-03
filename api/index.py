3e7e38b53e5e05f0ff8a1ed27c87cf55c2f1f0ff
TheBoringDude <iamcoderx@gmail.com>
2020-10-03 22:19:23 +0800

    Some fixes and removals

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ async def index(request, path=""):
 async def weather(request, city_id=""):
     weather = Weather.Get(city_id)
 
-    if weather[0] != 404:
+    if weather[0] != "404":
         __message = {
             "messages": [
                 {"text": f"Weather for *{weather[0]}, {weather[1]}* \n\nIt feels like *{weather[5]}°C* ({weather[4]}°C).\n\nThe weather is *{weather[2]}* ({weather[3]}) with a maximum temperature of *{weather[7]}°C* and a minimum temperature of *{weather[6]}°C*."}
@@ -30,5 +30,5 @@ async def weather(request, city_id=""):
 
     return json(__message)
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
--- api/weather.py
+++ api/weather.py
@@ -10,7 +10,7 @@ class Weather:
     def Get(city_id):
         resp = requests.get(base_url + city_id + "&appid=" + os.getenv("OPENWEATHER")).json()
 
-        if resp["cod"] == 404:
+        if resp["cod"] == "404":
             return [resp["cod"]]
 
         return [resp['name'], resp['sys']['country'], resp['weather'][0]['main'], resp['weather'][0]['description'], Weather.convert_temp(resp['main']['temp']), Weather.convert_temp(resp['main']['feels_like']), Weather.convert_temp(resp['main']['temp_min']), Weather.convert_temp(resp['main']['temp_max'])]
