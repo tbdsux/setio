df3a11ba13ac8bf70781dc84e914859118ed6f89
TheBoringDude <iamcoderx@gmail.com>
2020-10-03 20:51:21 +0800

    Add weather functionality

--- api/index.py
+++ api/index.py
@@ -1,8 +1,27 @@
 from sanic import Sanic
 from sanic.response import json
-app = Sanic()
+# utils
+from weather import Weather
+
+app = Sanic(name="setio")
 
 @app.route("/")
-@app.route("/<path:path>")
+@app.route("/<path:string>")
 async def index(request, path=""):
     return json({"hello": path})
+
+@app.route("/weather/<city_id:string>")
+async def weather(request, city_id=""):
+    weather = Weather.Get(city_id)
+    __message = {
+        "messages": [
+            {"text": f"Weather for *{weather[0]}, {weather[1]}*"},
+            {"text": f"It feels like *{weather[5]}C* _({weather[4]}C)_."},
+            {"text": f"The weather is *{weather[2]}* ({weather[3]}) with a maximum temperature of *{weather[7]}C* and a minimum temperature of *{weather[6]}C*."}
+        ]
+    }
+
+    return json(__message)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
