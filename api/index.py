4bebf22849041212a6a7c8d253d1efd15f215644
TheBoringDude <iamcoderx@gmail.com>
2020-10-03 20:59:50 +0800

    Make it one file

--- api/index.py
+++ api/index.py
@@ -1,7 +1,25 @@
 from sanic import Sanic
 from sanic.response import json
+
 # utils
-from weather import Weather
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?q="
+
+class Weather:
+    @staticmethod
+    def Get(city_id):
+        resp = requests.get(base_url + city_id + "&appid=" + os.getenv("OPENWEATHER")).json()
+
+        return [resp['name'], resp['sys']['country'], resp['weather'][0]['main'], resp['weather'][0]['description'], Weather.convert_temp(resp['main']['temp']), Weather.convert_temp(resp['main']['feels_like']), Weather.convert_temp(resp['main']['temp_min']), Weather.convert_temp(resp['main']['temp_max'])]
+
+    # conver the temperature from kelvin to celcius
+    @staticmethod
+    def convert_temp(kelvin_temp):
+        return round(kelvin_temp - 273.15)
 
 app = Sanic(name="setio")
 
